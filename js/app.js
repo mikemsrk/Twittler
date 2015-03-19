fe7e3c09eeae6373328245f89fe4ef23b93a6d55
mikemsrk <min2033@gmail.com>
2015-03-19 13:57:23 -0700

    Add user profile name & image update

--- js/app.js
+++ js/app.js
@@ -4,16 +4,20 @@ $(document).ready(function(){
 	setInterval(update,3000);
 
 	$('#tweets').on('click','a',function(e){
-		e.preventDefault();
+		e.preventDefault();	
+		//	Switch to user profile
 		var user = $(this).attr("href");
 		user = user.slice(1);
 		app.user = user;
+		app.profileImage = 'img/blank.jpg';
+	
 		showTweets();
 	});
 
 	$('#home').on('click',function(e){
 		e.preventDefault();
-		app.user = undefined;
+		app.user = 'hamstar';
+		app.profileImage = 'img/hamster.jpg';
 		showTweets();
 	});
 
@@ -26,19 +30,24 @@ $(document).ready(function(){
 });
 
 var app = {
-	user: undefined,
-	tweets: 0
+	user: 'hamstar',
+	tweets: 0,
+	viewLimit: 20,
+	profileImage: 'img/hamster.jpg'
 };
 
 var showTweets = function(){
 
 	var tweets, $body = $('#tweets');
 	$body.html('');
+	$('.profile img').attr('src',app.profileImage);
+	$('.profile a').text('@' + app.user);
 
-	app.user === undefined ? tweets = streams.home : tweets = streams.users[app.user];
+
+	app.user === 'hamstar' ? tweets = streams.home : tweets = streams.users[app.user];
 	app.tweets = tweets.length;
 
-	tweets = tweets.slice(-20);
+	tweets = tweets.slice(-app.viewLimit);
 
 	tweets.forEach(function(tweet){
 		var item = "<li>";
@@ -54,14 +63,16 @@ var showTweets = function(){
 };
 
 var update = function(){
+	//Update the "new tweets" bar
 	var upd = $('#update');
 	var tweets;
-	app.user === undefined ? tweets = streams.home : tweets = streams.users[app.user];
+	app.user === 'hamstar' ? tweets = streams.home : tweets = streams.users[app.user];
 
 	var newTweets = tweets.length - app.tweets;
 
 	newTweets === 0 ? upd.slideUp() : upd.slideDown();
 	var text = "<a href='#'>" + newTweets + ' new tweets.</a>';
+	upd.html(text);
 
 	//Update timestamps
 	var dates = $('#tweets abbr');
@@ -69,7 +80,5 @@ var update = function(){
 		var timestamp = $.timeago(new Date($(item).attr('title')));
 		$(item).text(timestamp);
 	});
-
-	upd.html(text);
 };
 
