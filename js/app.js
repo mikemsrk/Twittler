0ae8e2bd88f4dccbcfd0e48e3dbda4bc157398f5
mikemsrk <min2033@gmail.com>
2015-03-18 20:36:50 -0700

    Add timeago library, user click function, and reset function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
 	showTweets();
 	setInterval(showTweets,3000);
 
+	$('#tweets').on('click','a',function(e){
+		e.preventDefault();
+		var user = $(this).attr("href");
+		user = user.slice(1);
+		app.user = user;
+		showTweets();
+	});
+
+	$('#home').on('click',function(e){
+		e.preventDefault();
+		app.user = undefined;
+		showTweets();
+	});
+
+
 });
 
 var app = {
@@ -14,14 +29,17 @@ var showTweets = function(){
 	var tweets, $body = $('#tweets');
 	$body.html('');
 
-	app.user === undefined ? tweets = streams.home : tweets = streams.user;
+	app.user === undefined ? tweets = streams.home : tweets = streams.users[app.user];
 
 	tweets = tweets.slice(-20);
 
 	tweets.forEach(function(tweet){
-		var $tweet = $('<div></div>');
-		$tweet.text('@' + tweet.user + ': ' + tweet.message);
-		$tweet.prependTo($body);
+		var item = "<li>";
+		item += "<a href=#" + tweet.user + ">@" + tweet.user +"</a>: ";
+		item += "<span>" + tweet.message + "</span> ";
+		item += "<span class='date'> " + $.timeago(tweet.created_at) + "</span></li>";
+
+		$body.prepend(item);
 	});
 
 };
